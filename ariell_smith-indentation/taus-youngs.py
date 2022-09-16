c74e0cce59c1098eb68c3245a51c86d08c02deb6
Brandon Pardi <brandonpardi24@gmail.com>
2022-09-16 13:01:22 -0700

    tau file changes

--- ariell_smith-indentation/taus-youngs.py
+++ ariell_smith-indentation/taus-youngs.py
@@ -6,19 +6,18 @@ from scipy.optimize import curve_fit
 
 
 df = pd.read_csv("aggregate_data/taus-youngs.csv")
+#df = df.drop_duplicates()
 print([df['Tau'][df['data_category'] == "soft"].values])
-data_dict = {
-    'tau_soft':[df['Tau'][df['data_category'] == "soft"].values],
-    'tau_stiff':[df['Tau'][df['data_category'] == "soft"].values],
-    'E_soft':[df['Tau'][df['data_category'] == "soft"].values],
-    'E_stiff':[df['Tau'][df['data_category'] == "soft"].values]
-}
 
 
-data_df = pd.DataFrame(data_dict)
-print(data_df.head())
+soft_df = df[df['data_category'] == "soft"]
+stiff_df = df[df['data_category'] == "stiff"]
+soft_viscoelastic_df = df[df['data_category'] == "soft_viscoelastic"]
+stiff_viscoelastic_df = df[df['data_category'] == "stiff_viscoelastic"]
+
+print(soft_df.head())
 
 plt.figure(1)
-sns.swarmplot(data=data_df)
-sns.boxplot(data=data_df, boxprops={'facecolor':'None'})
+sns.swarmplot(data=soft_df)
+sns.boxplot(data=soft_df, boxprops={'facecolor':'None'})
 plt.savefig("taus-youngs_plots/swarmplot.png")
