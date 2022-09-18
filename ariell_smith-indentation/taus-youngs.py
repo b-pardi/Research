237a79c7dec4460559881b120a1122ffae959e73
brandonator24 <72064522+brandonator24@users.noreply.github.com>
2022-09-18 16:45:13 -0700

    swarm plots working correctly

--- ariell_smith-indentation/taus-youngs.py
+++ ariell_smith-indentation/taus-youngs.py
@@ -1,7 +1,7 @@
 """
 Author: Brandon Pardi
 Created: 9/14/2022, 12:45 pm
-Last Modified: 9/16/2022 9:34pm
+Last Modified: 9/18/2022 4:44pm
 """
 
 import pandas as pd
@@ -21,32 +21,41 @@ README
     - each figure will contain a plot for soft, stiff, soft_viscoelastic, stiff_viscoelastic
 
 WIP
-- like all of it really
 - currently viscoelastic data does not exist yet.
 '''
 
 df = pd.read_csv("aggregate_data/taus-youngs.csv")
 df = df.drop_duplicates()
-df.to_csv("aggregate_data/taus-youngs.csv")
+#df.to_csv("aggregate_data/taus-youngs.csv")
 
-#print([df['Tau'][df['data_category'] == "soft"].values])
-
-
-soft_df = df[df['data_category'] == "soft"]
-stiff_df = df[df['data_category'] == "stiff"]
-soft_viscoelastic_df = df[df['data_category'] == "soft_viscoelastic"]
-stiff_viscoelastic_df = df[df['data_category'] == "stiff_viscoelastic"]
-
-tau_data_dict = {
-    'soft':soft_df['Tau'].values,
-    'stiff':stiff_df['Tau'].values
-}
+tau_df = df[['Tau', 'data_category']]
+tau_rsq_df = df[['T_rsq', 'data_category']]
+E_df = df[['E', 'data_category']]
+E_rsq_df = df[['E_rsq', 'data_category']]
 
 '''
 figure 1: Taus
-figure 2: Youngs
+figure 2: Tau rsq
+figure 3: Youngs Modulus
+figure 4: Youngs mod rsq
 '''
+
 plt.figure(1)
-sns.swarmplot(data=tau_data_dict)
-sns.boxplot(x=tau_data_dict['soft'], boxprops={'facecolor':'None'})
-plt.savefig("taus-youngs_plots/swarmplot.png")
+sns.swarmplot(x='data_category', y='Tau', data=tau_df)
+sns.boxplot(x='data_category', y='Tau', data=tau_df, boxprops={'facecolor':'None'})
+plt.savefig("taus-youngs_plots/TAUS-swarmplot.png")
+
+plt.figure(2)
+sns.swarmplot(x='data_category', y='T_rsq', data=tau_rsq_df)
+sns.boxplot(x='data_category', y='T_rsq', data=tau_rsq_df, boxprops={'facecolor':'None'})
+plt.savefig("taus-youngs_plots/TAUS-RSQ-swarmplot.png")
+
+plt.figure(3)
+sns.swarmplot(x='data_category', y='E', data=E_df)
+sns.boxplot(x='data_category', y='E', data=E_df, boxprops={'facecolor':'None'})
+plt.savefig("taus-youngs_plots/YOUNGS-swarmplot.png")
+
+plt.figure(4)
+sns.swarmplot(x='data_category', y='E_rsq', data=E_rsq_df)
+sns.boxplot(x='data_category', y='E_rsq', data=E_rsq_df, boxprops={'facecolor':'None'})
+plt.savefig("taus-youngs_plots/YOUNGS-RSQ-swarmplot.png")
